a267f8d8dc6fad03c7d1300e29f9da9d50150f6e
Mike Roberts <mike.roberts.2k10@googlemail.com>
2019-10-10 11:03:44 +0100

    init comment

--- breathe/__init__.py
+++ breathe/__init__.py
@@ -1,11 +1,12 @@
 from .grammar.master import Master
 from .elements import CommandContext
-import sys
+import sys as _sys
 
-if not hasattr(sys, "_called_from_test"):
+if not hasattr(_sys, "_called_from_test"):
+    # Some discussion here of whether this pattern is a good idea, I think it's fine for now.
+    # https://stackoverflow.com/questions/9561042/python-init-py-and-initialization-of-objects-in-a-code
     Breathe = Master()
 else:
     from dragonfly import get_engine
     engine = get_engine("text")
     Breathe = Master(engine=engine)
-
