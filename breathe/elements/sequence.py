596c03309bf3c3e202f7debe8707a2901a78108e
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-10-14 20:50:55 +0100

    Add sequences

---
+++ breathe/elements/sequence.py
@@ -0,0 +1,21 @@
+from dragonfly import Repetition, ActionBase, Empty
+
+
+class Sequence(Repetition):
+    def __init__(self, name, max=4, min=1, default=None):
+        Repetition.__init__(
+            self, child=Empty(), min=min, max=max, name=name, default=default
+        )
+
+
+class ExecSequence(ActionBase):
+    def __init__(self, name):
+        self._name = name
+        ActionBase.__init__(self)
+
+    def _execute(self, data=None):
+        action_list = data[self._name]
+        if not isinstance(action_list, list):
+            raise TypeError
+        for action in action_list:
+            action.execute()
